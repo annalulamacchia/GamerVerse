2fe7b60e29f382bc4a5f8ce73bdfc66645bcd935
jackbab0 <giacomo.babudri@gmail.com>
2024-11-11 02:29:10 +0100

    Added profile_post_page

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../views/profile_post_page.dart';
+import '../views/profile_page.dart';
 
 class TabBarSection extends StatelessWidget {
   const TabBarSection({Key? key}) : super(key: key);
@@ -10,34 +12,68 @@ class TabBarSection extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildTabButton('Games', Colors.green),
-          _buildTabButton('Reviews', Colors.black),
-          _buildTabButton('Post', Colors.black),
+          _buildTabButton(
+            context,
+            'Games',
+            Colors.green,
+                () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
+          ),
+          _buildTabButton(
+            context,
+            'Reviews',
+            Colors.black,
+                () {
+              //Navigator.push(
+               // context,
+                //MaterialPageRoute(builder: (context) => ReviewsPage()),
+              //);
+            },
+          ),
+          _buildTabButton(
+            context,
+            'Post',
+            Colors.black,
+                () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePostPage() ),
+              );
+            },
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildTabButton(String text, Color color) {
+  Widget _buildTabButton(
+      BuildContext context, String text, Color color, VoidCallback onTap) {
     return Expanded(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 4.0),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(8.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 4,
-              offset: const Offset(0, 2),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          margin: const EdgeInsets.symmetric(horizontal: 4.0),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
-          ],
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ),
       ),
